79b9de0e256908bab0a2d0d3991a7cdf9c390596
Andrius B <tyroxy@gmail.com>
2022-01-18 23:01:38 +0200

    Array

---
+++ js/02-array.js
@@ -0,0 +1,48 @@
+// Array - masyvas, sarasas, listas, matrica
+
+const maistoSarasas = ['pomidoras', 'agurkas', 'bulve', 'svogunas']
+console.log(maistoSarasas);
+
+console.log('---------------------------------------');
+
+const pazymiai = [10, 2, 8, 6, 4]
+console.log(pazymiai)
+
+console.log('---------------------------------------');
+
+
+console.log('Reikia nupirkti:', 'pomidoras');
+console.log('Reikia nupirkti:', 'agurkas');
+console.log('Reikia nupirkti:', 'bulve');
+console.log('Reikia nupirkti:', 'svogunas');
+
+console.log('---------------------------------------');
+
+console.log('Reikia nupirkti:', maistoSarasas[1]);
+console.log('Reikia nupirkti:', maistoSarasas[2]);
+console.log('Reikia nupirkti:', maistoSarasas[3]);
+console.log('Reikia nupirkti:', maistoSarasas[4]);
+
+console.log('---------------------------------------');
+
+// Pazymiai: 10, 2, 8, 6, 4.
+// Pazymiai: 30.
+// Pazymiai: 5.
+// Pazymiu vidurkis: 6.
+
+console.log('---------------------------------------');
+
+const asmuo1 = 'Petras';
+const asmuo2 = 'Ona';
+const asmuo3 = 'Jonas';
+const asmuo4 = 'Maryte';
+
+const asmenys =[asmuo1, asmuo2, asmuo3, asmuo4];
+console.log(asmenys);
+
+//Klaseje mokosi 4 asmenys.
+//Mokiniu vardai yra Petras, Ona, Jonas, Maryte.
+
+console.log('---------------------------------------');
+
+
