a4e2de987bca5213ee5752cbccdd606934dcfe01
Andrius B <tyroxy@gmail.com>
2022-01-18 16:01:32 +0200

    Variables

---
+++ js/01-variables.js
@@ -0,0 +1,67 @@
+console.log('Kintamieji');
+
+
+const pirmas = 5;
+const antras = 7;
+
+console.log(pirmas);
+console.log(antras);
+
+console.log(antras);
+console.log(pirmas);
+
+
+
+const vardas = 'Vardenis';
+const pavarde = "Pavardenis";
+
+console.log(vardas);
+console.log(pavarde);
+
+const adresas = 'Gatves g., 12-34, Miestas, Valstybe, VA-123456';
+console.log(adresas)
+
+console.log('---------------------------------------')
+
+console.log('Pinigineje yra:')
+let pinigine = 0;
+console.log(pinigine)
+
+const pirmasPinigas = 5;
+console.log('Pirmas pinigas:' , pirmasPinigas);
+
+pinigine = pinigine + pirmasPinigas
+console.log('Pinigineje yra:' , pinigine);
+
+const antrasPinigas = 20;
+console.log('Antras pinigas:' , antrasPinigas);
+
+const treciasPinigas = 50;
+console.log('Trecias pinigas:' , treciasPinigas);
+
+const pirmaIslaida = 40;
+console.log('Pirma islaida:' , pirmaIslaida);
+
+const antraIslaida = 50;
+console.log('Antra islaida:' , antraIslaida);
+
+
+pinigine = pinigine + antrasPinigas + treciasPinigas - pirmaIslaida - antraIslaida
+console.log('Pinigineje yra:' , pinigine);
+
+
+console.log('---------------------------------------')
+
+const pirmasZodis = 'Pirmas';
+const antrasZodis = 'zodis';
+
+const sakinys = pirmasZodis + ' ' + antrasZodis
+console.log(1, pirmasZodis);
+console.log(2, antrasZodis);
+console.log(3, sakinys);
+
+
+
+console.log('---------------------------------------')
+
+
