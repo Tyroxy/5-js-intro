48009028e9db8de5e22b6043cb713c461f9adf69
Andrius B <tyroxy@gmail.com>
2022-01-19 20:43:37 +0200

    Switch

---
+++ js/04-switch.js
@@ -0,0 +1,62 @@
+const diena = 1
+
+switch (diena) {
+    case 1:
+        console.log('pirmadienis');
+        // break;
+    case 2:
+         console.log('antradienis');
+
+    case 3:
+        console.log('treciadienis');
+
+    case 4:
+        console.log('ketvirtadienis');
+            break;
+    case 5:
+        console.log('penktadienis');
+    
+    case 6:
+        console.log('sestadienis');
+
+    case 7:
+        console.log('sekmadienis');
+}
+
+console.log('...................................')
+
+const dd = 8
+
+switch (dd) {
+    case 1:
+        console.log('darbo diena');
+        break;
+    case 2:
+         console.log('darbo diena');
+        break;
+
+    case 3:
+        console.log('darbo diena');
+        break;
+
+    case 4:
+        console.log('darbo diena');
+        break;
+    case 5:
+        console.log('darbo diena');
+        break;
+
+    case 6:
+        console.log('savaitgalis');
+        break;
+
+    case 7:
+        console.log('ssavaitgalis');
+        break;
+
+    default:
+        console.log('tokia diena neegzistuoja');
+}
+
+console.log('...................................')
+
